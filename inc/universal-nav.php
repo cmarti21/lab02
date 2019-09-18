cb5c4a4b3e349ad6840024999e7ffc0d1206bc3a
Ana Acuna <anasofacuna@gmail.com>
2019-09-18 14:18:53 -0400

    populated universal nav

---
+++ inc/universal-nav.php
@@ -0,0 +1,10 @@
+<nav>
+			<ul>
+				<li><a href="#">Home</a></li>
+				<li><a href="#">Joshua</a></li>
+				<li><a href="#">Maria</a></li>
+				<li><a href="#">Alyssa</a></li>
+			</ul>
+		</nav>
+
+	<nav>
