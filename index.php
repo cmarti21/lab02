bc2c51112923093ea61bfdb5d75897fbbc94825a
Ana Acuna <anasofacuna@gmail.com>
2019-09-20 19:35:04 -0400

    made index uniform with other pages. added footer as well

--- index.php
+++ index.php
@@ -12,13 +12,16 @@
 
 	<header>
 		<h1>Team Barcelona and Friends</h1>
-		<?php include 'inc/universal-nav.php';?>
 	</header>
+
+	<?php include 'inc/universal-nav.php';?>
 	
 	<main>
 		<h2>Back at it for another assignment...</h2>
 		<p>For this assignment, we're looking at our peers' Joshua, Maria, and Alyssa! We're excited to learn more about them and hope to share a new perspective on them with you.</p>
 	</main>
+
+	<footer>CSC 174: Advanced Front-end Web Design and Development</footer>
 	
 </body>
 </html>
