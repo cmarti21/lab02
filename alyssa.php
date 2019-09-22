a5c67ee98d513c3f96cad4a74dea85fb91b731a3
Andrew Thankson <athankso@u.rochester.edu>
2019-09-22 18:40:16 -0400

    homepage redesign

--- alyssa.php
+++ alyssa.php
@@ -21,10 +21,10 @@
 
 	<nav>
 		<ul>
-			<li><a href="/alyssa.php">Home</a></li>
-			<li><a href="/alyssa-high-school.php">High School</a></li>
-			<li><a href="/alyssa-early-college.php">Early College</a></li>
-			<li><a href="/alyssa-now.php">Now</a></li>
+			<li><a href="alyssa.php">Home</a></li>
+			<li><a href="alyssa-high-school.php">High School</a></li>
+			<li><a href="alyssa-early-college.php">Early College</a></li>
+			<li><a href="alyssa-now.php">Now</a></li>
 		</ul>
 	</nav>
 
