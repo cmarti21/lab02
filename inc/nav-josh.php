7f36bae7ddb9a5074307546d5cb3fad613caeea9
Clara Martinez <cmarti21@u.rochester.edu>
2019-09-22 23:48:33 -0400

    php includes

---
+++ inc/nav-josh.php
@@ -0,0 +1,8 @@
+	<nav>
+		<ul>
+			<li><a  href="joshua.php">Home</a></li>
+			<li><a  href="joshua-jobs.php">Jobs</a> </li>
+			<li><a  href="joshua-travel.php">Travel</a></li>
+			<li><a  href="joshua-tidbits.php">Tidbits</a></li>
+		</ul>
+	</nav>
---
+++ inc/nav-maria.php
@@ -0,0 +1,8 @@
+	<nav>
+		<ul>
+			<li><a href="maria.php">Home</a></li>
+			<li><a href="maria-high-school.php">High School</a></li>
+			<li><a href="maria-university.php">University</a></li>
+			<li><a href="maria-hobbies.php">Hobbies</a></li>
+		</ul>
+	</nav>
